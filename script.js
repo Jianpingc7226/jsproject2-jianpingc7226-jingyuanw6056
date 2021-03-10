b4b9d4d009f624f955abb6787d048594e067dda3
Jing Yuan Wang <jingyuanw6056@hstat.org>
2021-03-10 15:53:31 +0000

    test

--- script.js
+++ script.js
@@ -2,61 +2,13 @@ $.ajax({
     url: 'https://data.cityofnewyork.us/resource/rc75-m7u3.json',
     dataType: 'json',
     success: function(data) {
-        console.log(data)
+        var totalDeath = 0;
+        var totalCases = 0;
         
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
+        document.write(data.length);
+        for (var i = 0 ; i < data.length; i++) {
+            totalDeath = totalDeath + data[i].death_count;
+            document.write(totalDeath)
+        }
+    } 
 });
