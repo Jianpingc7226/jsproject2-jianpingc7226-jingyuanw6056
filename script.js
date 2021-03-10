b3e9eb5ce2edcf0a16f2d43e1b32292228f86fb2
Jianping Chen <jianpingc7226@hstat.org>
2021-03-10 17:44:03 +0000

    half way done

--- script.js
+++ script.js
@@ -3,27 +3,17 @@ $.ajax({
     dataType: 'json',
     success: function(data) {
 
-        $.ajax({
-    url: 'https://data.cityofnewyork.us/resource/rc75-m7u3.json',
-    dataType: 'json',
-    success: function(data) {
-
       var userDate = prompt("What date you want to go?(please enter in this form YYYY-MM-DD");
 
       for(i=0;i<data.length;i++){
-
-        var date = data[i].date_of_interest
+        var deathCount = data[i].death_count;
+        var caseCount = data[i].case_count;
+        var date = data[i].date_of_interest;
         if(date.substring(0,10) == userDate){
-          console.log("good")
+          document.write("the death count of that day is " + deathCount + ".</br>");
+          document.write("the case count of that day is " + caseCount + ".");
           return
-          } else {
-            console.log("bad")
-            return
-          };       
+          };
       }
     }
-});
-        
-        
-    } 
 });
