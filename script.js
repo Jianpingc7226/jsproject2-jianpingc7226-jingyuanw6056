a647c2d4d9f22ae3e15e993a9b2a4f77d2d764c7
Jianping Chen <jianpingc7226@hstat.org>
2021-03-10 15:54:35 +0000

    update 1

--- script.js
+++ script.js
@@ -2,13 +2,28 @@ $.ajax({
     url: 'https://data.cityofnewyork.us/resource/rc75-m7u3.json',
     dataType: 'json',
     success: function(data) {
-        var totalDeath = 0;
-        var totalCases = 0;
+
+        $.ajax({
+    url: 'https://data.cityofnewyork.us/resource/rc75-m7u3.json',
+    dataType: 'json',
+    success: function(data) {
+
+      var userDate = prompt("What date you want to go?(please enter in this form YYYY-MM-DD");
+
+      for(i=0;i<data.length;i++){
+
+        var date = data[i].date_of_interest
+        if(date.substring(0,10) == userDate){
+          console.log("good")
+          return
+          } else {
+            console.log("bad")
+            return
+          };       
+      }
+    }
+});
         
-        document.write(data.length);
-        for (var i = 0 ; i < data.length; i++) {
-            totalDeath = totalDeath + data[i].death_count;
-            document.write(totalDeath)
-        }
+        
     } 
 });
