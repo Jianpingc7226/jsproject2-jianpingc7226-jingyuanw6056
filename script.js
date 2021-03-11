dfd35654e2d3a009dc00ea71eb0a2f10b78478d3
Jing Yuan Wang <jingyuanw6056@hstat.org>
2021-03-11 03:13:11 +0000

    comments added for explaination of the code

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ $.ajax({
     url: 'https://data.cityofnewyork.us/resource/rc75-m7u3.json',
     dataType: 'json',
     success: function(data) {
-      var userOption = prompt("Do wish to view the overal data? (Input 1). Or view a specific date? (Input 2, please only enter 1 or 2)")
+      var userOption = prompt("Do wish to view the overal data? (Input 1). Or view a specific date? (Input 2, please only enter 1 or 2)") //gives the user some options to choose from
       
       if (userOption == 1) {
         var tdeathCount = 0;
-        var tcaseCount = 0;
+        var tcaseCount = 0; //starting values
         for(i = 0; i < data.length; i++){
-          var deathCount = parseInt(data[i].death_count);
+          var deathCount = parseInt(data[i].death_count); //converts string into int where it can be added up
           tdeathCount = tdeathCount + deathCount
-          var caseCount = parseInt(data[i].case_count);
+          var caseCount = parseInt(data[i].case_count); //converts string into int where it can be added up
           tcaseCount = tcaseCount + caseCount
         }
         document.write("The death count in total is " + tdeathCount + ".</br>");
@@ -21,22 +21,22 @@ $.ajax({
         var userDate = prompt("What date you want to go? (Please enter in this in form of YYYY-MM-DD");
         for(i = 0; i < data.length; i++){
           var deathCount = data[i].death_count;
-          var caseCount = data[i].case_count;
+          var caseCount = data[i].case_count; //we use this to simplify things up
           var date = data[i].date_of_interest;
           var checkPoint = 0;
-          if (date.substring(0,10) == userDate){
+          if (date.substring(0,10) == userDate){ //Jian Ping did a very good job at this part :D
             document.write("The death count of " + userDate + " is " + deathCount + ".</br>");
             document.write("The case count of " + userDate + " is " + caseCount + ".");
             checkPoint ++;
           }
         }
-        if (checkPoint == 0) {
-          document.write("Invaild input or date is out of bounds/undocumented, please refresh the page and try again.")
+        if (checkPoint == 0) { //this is used to check whether or not the user input a vaild input. If not, beep- you got a friendly notice
+          document.write("Invaild input or date is out of bounds/undocumented, please refresh the page and try again.") //Incase the user put something wrong, here is a notice message
         }
       }
       
       else {
-        document.write("Invaild input, please refresh the page and try again.")
+        document.write("Invaild input, please refresh the page and try again.") //Incase the user put something wrong, here is a notice message, again
       }
     }
 });
